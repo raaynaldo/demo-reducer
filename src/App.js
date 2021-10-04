de366202468e809213894068052a5af476884948
Raynaldo Sutisna <raaynaldo@gmail.com>
2021-10-04 13:12:52 -0400

    feat: demo reducer done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useState } from 'react';
+
+const counterInitialState = { counter: 0 };
+
+function counterReducer(state, action) {
+  const { type, payload } = action;
+
+  console.log('State', state);
+  console.log('Action ', action);
+  switch (type) {
+    case 'decrementTen':
+      return { counter: state.counter - 10 };
+    case 'decrement':
+      return { counter: state.counter - 1 };
+    case 'increment':
+      return { counter: state.counter + +payload.input };
+    case 'incrementTen':
+      return { counter: state.counter + 10 };
+    case 'resetToInital':
+      return { counter: 0 };
+    default:
+      throw new Error();
+  }
+}
 
 function App() {
+  const [counterState, counterDispatch] = useReducer(
+    counterReducer,
+    counterInitialState
+  );
+
+  const [input, setInput] = useState(0);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'Flex', justifyContent: 'center' }}>
+      <button
+        onClick={() =>
+          counterDispatch({ type: 'decrementTen', payload: { one: 1, two: 2 } })
+        }
+      >
+        -10
+      </button>
+      <button onClick={() => counterDispatch({ type: 'decrement' })}>-</button>
+      <div>{counterState.counter}</div>
+      <button
+        onClick={() =>
+          counterDispatch({ type: 'increment', payload: { input: input } })
+        }
+      >
+        +
+      </button>
+      <button onClick={() => counterDispatch({ type: 'incrementTen' })}>
+        +10
+      </button>
+      <button onClick={() => counterDispatch({ type: 'resetToInital' })}>
+        Reset to Initial
+      </button>
+      <input
+        type='text'
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
     </div>
   );
 }
 
 export default App;
+
+// const decrementTen = () => {
+//   setCounter(counter - 10);
+// };
+
+// const decrement = () => {
+//   setCounter(counter - 1);
+// };
+
+// const increment = () => {
+//   setCounter(counter + 1);
+// };
+
+// const incrementTen = () => {
+//   setCounter(counter + 10);
+// };
+
+// const resetToInitial = () => {
+//   setCounter(0);
+// };
